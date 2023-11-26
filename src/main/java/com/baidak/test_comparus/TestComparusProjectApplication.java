4973dbbd8fa9265f09e38ac703754415128ac437
Leonid Baidak <leonid3000399@gmail.com>
2023-11-26 17:23:32 +0200

    Added standard implementation of DataSource

--- src/main/java/com/baidak/test_comparus/TestComparusProjectApplication.java
+++ src/main/java/com/baidak/test_comparus/TestComparusProjectApplication.java
@@ -2,9 +2,8 @@ package com.baidak.test_comparus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class TestComparusProjectApplication {
 
     public static void main(String[] args) {
